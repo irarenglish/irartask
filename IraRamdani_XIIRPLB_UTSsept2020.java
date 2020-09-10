d2524758050e2b85ddaf1e0a0efe408629fd7215
irar <71078322+irarenglish@users.noreply.github.com>
2020-09-10 21:46:44 +0700

    Add files via upload

---
+++ IraRamdani_XIIRPLB_UTSsept2020.java
@@ -0,0 +1,50 @@
+package jFoTest;
+
+/**
+ * irar
+ * java foundie
+ * september 8, 2020
+ */
+
+public class IraRamdani_XIIRPLB_UTSsept2020 {
+	public static void main(String[] args) {
+
+        String word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+        
+        //charAt
+        System.out.print(word.charAt(8));
+        System.out.print(word.charAt(17));
+        System.out.print(word.charAt(0));
+        System.out.print(word.charAt(26));
+        System.out.print(word.charAt(17));
+        System.out.print(word.charAt(0));
+        System.out.print(word.charAt(12));
+        System.out.print(word.charAt(3));
+        System.out.print(word.charAt(0));
+        System.out.print(word.charAt(13));
+        System.out.println(word.charAt(8));
+        
+        //length
+		String text = "5678910111213" ;	
+		 int ttl = text.length();
+		   System.out.print("DATE OF BIRTH : " + ttl);
+        
+        //indexOf
+	    String me = new String("NOVEMBER ");
+	    String myself = new String("Date of Birth");
+	    System.out.print(" "+ me);
+
+	    
+	    //concat
+	       String str1 = "1";
+	       String str2 = "9";
+	       String str3 = "4";
+	       String str4 = "5";
+
+	       System.out.print(str1);
+	       System.out.print(str2);
+	       System.out.print(str3);
+	       System.out.print(str4);
+	    
+	}
+}
